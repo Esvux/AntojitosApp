9df4d98faf4841fb7c81a8de833441f38cd0e6bb
Esvin González <egonzalez@nabenik.com>
2018-11-13 17:11:05 -0600

    Agregando autenticación con correo & contraseña, Google y Facebook.

--- app/src/main/java/com/nabenik/antojitos/MainActivity.java
+++ app/src/main/java/com/nabenik/antojitos/MainActivity.java
@@ -1,13 +1,92 @@
 package com.nabenik.antojitos;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static int RC_SIGN_IN = 2030;
+
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        showUserName();
+        findViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doLogin();
+            }
+        });
+
+        findViewById(R.id.btn_logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doLogout();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode != RESULT_OK)
+            return;
+        if(requestCode == RC_SIGN_IN) {
+            user = FirebaseAuth.getInstance().getCurrentUser();
+            showUserName();
+        }
+    }
+
+    private void showUserName() {
+        if(user != null) {
+            findViewById(R.id.btn_login).setVisibility(View.INVISIBLE);
+            ((TextView)findViewById(R.id.tv_hello)).setText(
+                    String.format("Hola %s!!!", user.getDisplayName())
+            );
+        }
+    }
+
+    private void doLogin() {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build()
+        );
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setTheme(R.style.NoActionBar)
+                        .build(),
+                RC_SIGN_IN
+        );
+    }
+
+    private void doLogout() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                    }
+                });
     }
 }
